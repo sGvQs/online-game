720dc87cd972fc1381b029dbbb95a97941b07fad
su-goto <susum.earth@gmail.com>
2026-03-25 13:29:59 +0900

    wip

--- src/components/ui/modal/index.tsx
+++ src/components/ui/modal/index.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect } from "react";
 import { createPortal } from "react-dom";
-import { modal } from "./modal.styles";
+import { modal } from "./styles";
 import { Button } from "@/components/ui/button";
 import { Typography } from "@/components/ui/typography";
 
