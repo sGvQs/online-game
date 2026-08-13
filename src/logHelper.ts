d7a34229645bef9140a88bfb079faf3baf7b74e6
su-goto <susum.earth@gmail.com>
2026-08-13 17:04:47 +0900

    メジャーファイルの削除

--- src/logHelper.ts
+++
@@ -1,9 +0,0 @@
-// ログ用のヘルパー関数
-export const getDetailedTime = () => {
-	const now = new Date();
-	const h = String(now.getHours()).padStart(2, "0");
-	const m = String(now.getMinutes()).padStart(2, "0");
-	const s = String(now.getSeconds()).padStart(2, "0");
-	const ms = String(now.getMilliseconds()).padStart(3, "0");
-	return `${h}:${m}:${s}.${ms}`;
-};
